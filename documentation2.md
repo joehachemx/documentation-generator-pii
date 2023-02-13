60d73f6432cdcd14058a044e5e2e4ec58478b1db
Michelle <michellemetni@gmail.com>
2023-02-13 14:06:04 +0200

    file code to MD

---
+++ documentation2.md
@@ -0,0 +1,34 @@
+~~~js
+let a=8;
+if(a>6) {
+    console.log("Hello world ");
+    a=7
+}
+~~~ 
+
+This code outputs Hello World if the value of a is greater than 6
+
+<br>
+
+~~~js
+let a=7
+~~~ 
+
+this stores 7 in a variable called a
+
+<br>
+
+~~~js
+let s=0 
+let n=10 
+for (let i=0; i<=n, i++,) {
+    if (n%2==0){
+       s=s+1
+      }
+   }
+~~~ 
+
+hi this is a comment for the code above.
+
+<br>
+
---
+++ filetoMD2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+object={
+    "let a=8;Σif(a>6) {Σ    console.log(\"Hello world \");Σ    a=7Σ}":"This code outputs Hello World if the value of a is greater than 6",
+    "let a=7":"this stores 7 in a variable called a",
+    "let s=0 Σlet n=10 Σfor (let i=0; i<=n, i++,) {Σ    if (n%2==0){Σ       s=s+1Σ      }Σ   }" : "hi this is a comment for the code above."
+}
+
+function codetoMD(object){
+    fs.writeFileSync("documentation2.md","")
+    for (const key in object){
+        code=key.replace(/Σ/g, "\n");
+fs.appendFileSync("documentation2.md",`~~~js
+${code}
+~~~ \n
+${object[key]}\n\n<br>\n\n`)
+    }
+}
+
+codetoMD(object)
