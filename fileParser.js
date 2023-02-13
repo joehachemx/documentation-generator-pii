99e3560c91dba129d9bebe044efb2eeeb1e7a718
Jennifer Farah <jenniferfarah2003@gmail.com>
2023-02-13 18:12:21 +0200

    did half of the code

--- fileParser.js
+++ fileParser.js
@@ -1,1 +1,43 @@
-alert("hello world")
+const fs = require('fs');
+const { it } = require('node:test');
+const { isModuleNamespaceObject } = require('util/types');
+
+function fileParser(file) {
+    fs.readFile(file, 'utf-8', (err, data) => {
+        if (err) throw err;
+
+        let isReadingFunction = false
+
+
+        const lines = data.split('\n');
+    
+        lines.forEach((line) => {
+            if (line.includes("@endfunc")) {
+                isReadingFunction = false
+            }
+
+            let isReadingLineComment = false
+
+            if (isReadingFunction) {
+                let comment = ''
+                for (let i = 0; i < line.length; i++) {
+                    if (line[i] === "#") {
+                        isReadingLineComment = true
+                    }
+                    if (isReadingLineComment) {
+                        comment += line[i]
+                    }
+                }
+                console.log(comment)
+            }
+
+            if (line.includes("@startfunc")) {
+                isReadingFunction = true
+            }
+
+        })
+    })
+}
+
+fileParser("example.abc")
+
