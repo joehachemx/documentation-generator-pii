27fbcb7d3c0b46f3723454c094dfae3cf957a40f
joehachemx <89245507+joehachemx@users.noreply.github.com>
2023-02-20 07:58:05 +0200

    item code creation + update powerpoint

--- fileParser.js
+++ fileParser.js
@@ -1,43 +1,112 @@
 const fs = require('fs');
-const { it } = require('node:test');
-const { isModuleNamespaceObject } = require('util/types');
 
 function fileParser(file) {
+    let arrayOfItemCode = []
+
+    let regStart = /(?<=@<r)\w+/
+    let regEnd = /(?<=@r>)\w+/
+
     fs.readFile(file, 'utf-8', (err, data) => {
         if (err) throw err;
 
         let isReadingFunction = false
-
+        let thisID;
 
         const lines = data.split('\n');
     
+
+
         lines.forEach((line) => {
-            if (line.includes("@endfunc")) {
-                isReadingFunction = false
-            }
+            if (line.includes("@/w")) {
+                let reggg = /@\/?w(\d+)(.*?)$/
+                thisID = reggg.exec(line)[1]
 
-            let isReadingLineComment = false
-
-            if (isReadingFunction) {
-                let comment = ''
-                for (let i = 0; i < line.length; i++) {
-                    if (line[i] === "#") {
-                        isReadingLineComment = true
-                    }
-                    if (isReadingLineComment) {
-                        comment += line[i]
+                for (let i = 0; i < arrayOfItemCode.length; i++) {
+                    if (arrayOfItemCode[i].getId() == thisID) {
+                        arrayOfItemCode[i].explication = reggg.exec(line)[2]
                     }
                 }
-                console.log(comment)
             }
 
-            if (line.includes("@startfunc")) {
-                isReadingFunction = true
-            }
 
+
+            if (line.includes("@<r")) {
+                thisID = regStart.exec(line)[0]
+                let isPresent = false
+
+                for (let i = 0; i < arrayOfItemCode.length; i++) {
+                    if (arrayOfItemCode[i].getId() == thisID) {
+                        isPresent = true
+                    }
+                }
+
+                if (!isPresent) {
+                    let item = new itemCode(thisID)
+
+                    let customRegID = `(?<=@<r${thisID})[\\s\\S]*?(?=@r>${thisID})`
+                    customRegID = new RegExp(customRegID)
+
+                    item.code = customRegID.exec(data)[0] // format avant de put ici
+                    
+                    arrayOfItemCode.push(item)
+                }
+            }
         })
+
+
+        console.log()
+        console.log(arrayOfItemCode)
     })
 }
 
-fileParser("example.abc")
 
+
+
+// rah ejwe le code, a arranger avant de commit
+
+class itemCode {
+    constructor(id, code, explication) {
+      this.id = id; 
+      this.code = code;
+      this.explication = explication;
+    }
+
+    getId() {
+        return this.id;
+    }
+}
+
+/*
+parser, lama il trouve un nv id 
+il check sil existe dans array
+    si existe, il update
+    sinon il create a new itemcode object
+
+to get start id (?<=@<r)\w+
+to get finish id (?<=@r>)\w+
+
+
+
+je extract mnel data block a block of code puis je le format
+regex /@<r500([\s\S]*?)@r>500/
+
+
+get text id @\/?w(\d+)
+@\/?w(\d+)(.*?)$ use this
+
+
+to do:
+- format the data !!!
+- support mutliple explication on seperate lines
+
+
+
+*/
+
+
+
+
+
+
+
+fileParser("example.abc")
